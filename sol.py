067022c8bfed059aec79c11e22b265ee1e3aa870
Knut Hühne <knut@k-nut.eu>
2013-11-27 20:12:18 +0100

    refactorings

--- sol.py
+++ sol.py
@@ -11,31 +11,34 @@ class Polynom:
 
     def __repr__(self):
         rep = []
-        for i in range(0, len(self.coefficients)):
-            rep.append(str(self.coefficients[i]) + 'x^' + str(i))
+        for index, coefficient in enumerate(self.coefficients):
+            rep.append("%ix^%i" % (coefficient, index))
         return ' + '.join(rep)
 
     def calculate_at(self, position):
         sol = 0
-        for i in range(0, len(self.coefficients)):
-            sol += self.coefficients[i] * position ** i
+        for index, coefficient in enumerate(self.coefficients):
+            sol += coefficient * position ** index
         return sol
 
 
 def createPolynomFromNull(nullstellen):
     grad = len(nullstellen) + 1
-    coefficients = [0 for stelle in nullstellen]
-    coefficients.append(0)
+    coefficients = [0 for pos in range(0, len(nullstellen) + 1)]
     for i in range(0, grad):
         comb = combinations(nullstellen, i)
         for parts in comb:
-            partialProduct = 1
-            for part in parts:
-                partialProduct *= part
-            coefficients[i] += partialProduct
+            coefficients[i] += product(parts)
         coefficients[i] *= (-1) ** (grad - i)
     coefficients.reverse()
     for nullstelle in nullstellen:
         assert Polynom(coefficients).calculate_at(nullstelle) == 0
 
     return Polynom(coefficients)
+
+
+def product(collection):
+    product = 1
+    for element in collection:
+        product *= element
+    return product
