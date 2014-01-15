809e91ec7b31816e7f50c07b1635869a80ecb010
Knut Hühne <knut@k-nut.eu>
2014-01-15 22:42:57 +0100

    add ability to add rows

--- matrix.py
+++ matrix.py
@@ -8,9 +8,34 @@ class Matrix():
         self.rowCount = rows
         self.setUpMatrix()
 
+    def __repr__(self):
+        return "\n".join([str(row) for row in self.matrix])
+
     def setUpMatrix(self):
         for row in range(self.rowCount):
             self.matrix.append([0] * self.columnCount)
 
     def getValue(self, column, row):
         return self.matrix[column - 1][row - 1]
+
+    """
+    def setColumn(self, column, values):
+        if len(values) != self.rowCount:
+            raise ValueError("The number of values does not equal the number"
+                             +"of rows. Should be %i but is %i" % (self.rowCount,
+                                                                   len(values)))
+
+        for position, value in enumerate(values):
+            self.setValue(position, column, value)
+    """
+
+    def setRow(self, row, values):
+        if len(values) != self.columnCount:
+            raise ValueError("The number of values does not equal the number"
+                             +"of columns. Should be %i but is %i" %
+                             (self.columnCount, len(values)))
+
+        self.matrix[row - 1] = values
+
+    def setValue(self, column, row, value):
+        self.matrix[column - 1][row - 1] = value
--- tests/matrixTests.py
+++ tests/matrixTests.py
@@ -4,24 +4,33 @@
 import unittest
 from ..matrix import Matrix
 
-
+"""
 class TestBaiscMatrix(unittest.TestCase):
-    columns = 2
-    rows = 3
-    matrix = Matrix(columns, rows)
-
     def test_representation(self):
-        self.assertEqual(self.matrix.columnCount, 2)
-        self.assertEqual(self.matrix.rowCount, 3)
+        columns = 2
+        rows = 3
+        matrix = Matrix(columns, rows)
+        self.assertEqual(matrix.columnCount, 2)
+        self.assertEqual(matrix.rowCount, 3)
 
-        for column in range(self.columns):
-            for row in range(self.rows):
-                self.assertEqual(self.matrix.getValue(column, row), 0)
-
+        for column in range(columns):
+            for row in range(rows):
+                self.assertEqual(matrix.getValue(column, row), 0)
 """
+
+class TestSettingValues(unittest.TestCase):
     def test_filling_matrix(self):
-        self.matrix.setColumn(1, [1, 2, 3])
-"""
+        columns = 2
+        rows = 3
+        matrix = Matrix(columns, rows)
+        #self.assertRaises(ValueError, matrix.setColumn, 1, [1, 2, 3, 4])
+        self.assertRaises(ValueError, matrix.setRow, 1, [1, 2, 3])
+        """
+        matrix.setColumn(1, [1, 2, 3])
+        matrix.setColumn(2, [4, 5, 6])
+        """
+        matrix.setRow(2, [2, 5])
+        print(matrix)
 
 if __name__ == "__main__":
     unittest.main()
