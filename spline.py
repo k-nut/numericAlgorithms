c2002fc88ce233a25bed9209831077fdd9ecb0ed
Knut Hühne <knut@k-nut.eu>
2014-01-24 13:31:44 +0100

    Fix it

--- spline.py
+++ spline.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 from matrix import Matrix
+
 
 class Spline():
     def __init__(self, xValues, yValues, firstDerivate, lastDerivate):
@@ -17,17 +19,21 @@ class Spline():
         self.calculate_ds()
 
         self.create_matrix()
+        #self.cancel_out_bottom_row()
+        self.calculate_new_lambdas()
+        self.calculate_new_ds()
+        self.calculate_Mis()
 
     def calculate_lambdas_and_mys(self):
         self.calculate_lambdas()
-        self.mys = []
-        for i in range(1, self.length - 1):
-            self.mys.append(1 - self.lambdas[i -1])
+        self.mys = [False]
+        for l in [1 - l for l in self.lambdas[1:]]:
+            self.mys.append(l)
+
         self.mys.append(1)  # myn is always 1 (capter 3 page 33)
 
     def calculate_lambdas(self):
         self.lambdas = [1]  # lambda0 is always 1 (capter 3 page 33)
-        print(self.length)
         for i in range(1, self.length - 1):
             self.lambdas.append((self.xValues[i + 1] - self.xValues[i]) /
                                 (self.xValues[i + 1] - self.xValues[i - 1])
@@ -72,11 +78,39 @@ class Spline():
 
     def create_matrix(self):
         self.matrix = Matrix(self.length, self.length)
-        print(len(self.lambdas))
+        #2s
         for i in range(1, self.matrix.rowCount + 1):
             self.matrix.setValue(i, i, 2)
+
+        #lambdas
         for i in range(1, self.matrix.rowCount):
             self.matrix.setValue(i, i + 1, self.lambdas[i - 1])
+
+        #µs
         for i in range(2, self.matrix.rowCount + 1):
-            self.matrix.setValue(i, i - 1, self.mys[i -2])
-            pass
+            self.matrix.setValue(i, i - 1, self.mys[i -1])
+
+    def calculate_new_lambdas(self):
+        self.new_lambdas = []
+        self.new_lambdas.append(self.lambdas[0] / 2.0)
+        for i in range(1, len(self.lambdas)):
+            self.new_lambdas.append(self.lambdas[i] /
+                                    (2 - self.new_lambdas[i -1] * self.mys[i])
+                                    )
+
+    def calculate_new_ds(self):
+        self.new_ds = []
+        self.new_ds.append(self.ds[0] / 2.0)
+        for i in range(1, len(self.ds)):
+            self.new_ds.append((self.ds[i] - self.new_ds[i -1] * self.mys[i])/
+                               (2 - self.new_lambdas[i -1] * self.mys[i])
+                               )
+
+    def calculate_Mis(self):
+        self.Mis = [0] * self.length
+        self.Mis[-1] = self.new_ds[-1]
+        for i in range(self.length - 2, -1, -1):
+            self.Mis[i] = (self.new_ds[i]
+                           - self.new_lambdas[i] * self.Mis[i + 1]
+                           )
+        self.Mis.reverse()
--- test_spline.py
+++ test_spline.py
@@ -7,15 +7,20 @@ from spline import Spline
 
 class TestBasicSpline(unittest.TestCase):
     def test_baisc(self):
-        x = [-3, -2, -1, 0, 1, 2, 3]
-        y = [5, 3, 1, 0, -1, 2, -2]
+        x = [0, 1, 2, 3]
+        y = [7, 3, 5, -2]
         firstderivate = -1
-        lastderivate = 1
+        lastderivate = 0
         spline = Spline(x, y, firstderivate, lastderivate)
-        self.assertEqual(spline.lambdas, [1, 0.5, 0.5, 0.5, 0.5, 0.5])
-        self.assertEqual(spline.mys, [0, 0.5, 0.5, 0.5, 0.5, 1])
-        print(spline.matrix)
-
+        self.assertEqual(spline.lambdas, [1, 0.5, 0.5])
+        self.assertEqual(spline.mys, [False, 0.5, 0.5, 1])
+        self.assertEqual(spline.ds, [-18.0, 18.0, -27.0, 42.0])
+        self.assertEqual(spline.new_lambdas, [0.5, 2.0 / 7, 7.0 / 26])
+        #self.assertEqual(spline.new_ds, [-9, 90 / 7.0, -18.0, 1])  # die 1 ist
+        #falsch und muss ersetzt werden
+        expectedMis = [104 / 3.0, -82 / 3.0, 62 /3.0, -58 / 3.0]
+        for i in range(len(expectedMis)):
+            self.assertAlmostEqual(expectedMis[i], spline.Mis[i])
 
 if __name__ == "__main__":
     unittest.main()
